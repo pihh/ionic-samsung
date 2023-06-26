50468165773824f6be42d4bcbaf61385c704d6fb
filipe <filipe.sa1@ibm.com>
2023-06-26 11:29:11 +0100

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ViewChild } from '@angular/core';
+import { MenuController, PopoverController } from '@ionic/angular';
+import { OuiDialogService } from '../services/oui-dialog.service';
+import { OuiModalService } from '../services/oui-modal.service';
+import { OuiPopoverBaseComponent } from '../components/popover/oui-popover-base/oui-popover-base.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,54 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    public dialogService: OuiDialogService,
+    public modalService: OuiModalService,
+    public popoverController: PopoverController,
+    public menuController: MenuController
+  ) {}
 
-  constructor() {}
+  onHeaderButtonClick($event: any) {
+    console.log('onHeaderButtonClick', $event);
+    if ($event.name == 'add') {
+      this.onHeaderButtonClickAdd();
+    } else if ($event.name == 'search') {
+      this.onHeaderButtonClickSearch();
+    } else if ($event.name == 'ellipsis') {
+      this.onHeaderButtonClickEllipsis($event);
+    }else{
+      this.onHeaderButtonClickMenu($event)
+    }
+  }
 
+  async onHeaderButtonClickAdd() {
+    const { data, role } = await this.dialogService.open('base');
+    console.log({ data, role });
+  }
+
+  async onHeaderButtonClickSearch() {
+    const { data, role } = await this.modalService.open('base');
+    console.log({ data, role });
+  }
+  async onHeaderButtonClickEllipsis(e: any) {
+    const popover = await this.popoverController.create({
+      component: OuiPopoverBaseComponent,
+      event: e,
+    });
+
+    await popover.present();
+
+    const { role } = await popover.onDidDismiss();
+    console.log(role);
+  }
+
+  async onHeaderButtonClickMenu(e:any){
+    console.log(this.menuController)
+    this.menuController.open()
+  }
 }
+
+/*
+ 0.1 -> 1
+ 0.5 -> 0
+*/
